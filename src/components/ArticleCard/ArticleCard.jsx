3d7dfb8aa92cb4182a027975c74cd5effc49152c
Nayeem Uddin <unayeem33@gmail.com>
2024-11-02 08:13:19 +0600

    s

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -1,26 +1,33 @@
+import PropTypes from "prop-types";
+
 const ArticleCard = ({ article }) => {
   console.log(article);
   return (
     <>
       <div className="bg-gray-800 p-5 rounded-lg shadow-lg transition-transform transform hover:scale-105 relative overflow-hidden">
         <img
-          src="https://via.placeholder.com/150"
+          src={article?.image_url}
           alt="Article Title"
           className="w-full h-40 object-cover rounded-lg mb-4"
         />
-        <h3 className="text-xl font-semibold text-white mt-4">Article Title</h3>
-        <p className="text-gray-400">
-          Brief summary of the article goes here. This is where you capture the
-          reader&lsquo;s interest.
+        <h3
+          className="text-xl font-semibold text-white mt-4 h-14 overflow-hidden"
+          title={article?.title}
+        >
+          {article?.title}
+        </h3>
+        <p className="text-gray-400 h-12 overflow-hidden">{article?.summary}</p>
+        <p className="text-gray-500 text-sm mt-2">
+          Published on: {article?.published_at}
         </p>
-        <p className="text-gray-500 text-sm mt-2">Published on: YYYY-MM-DD</p>
 
-        <span className="absolute top-3 right-3 bg-accentColor2 text-white text-xs font-semibold px-2 py-1 rounded flex items-center shadow-md">
-          <i className="fas fa-star mr-1"></i> Featured
-        </span>
-
+        {article?.featured && (
+          <span className="absolute top-3 right-3 bg-accentColor2 text-white text-xs font-semibold px-2 py-1 rounded flex items-center shadow-md">
+            <i className="fas fa-star mr-1"></i> Featured
+          </span>
+        )}
         <a
-          href="https://example.com/article"
+          href={article?.url}
           target="_blank"
           rel="noopener noreferrer"
           className="text-accentColorHover hover:text-gray-500 mt-4 inline-block font-semibold"
@@ -30,6 +37,10 @@ const ArticleCard = ({ article }) => {
       </div>
     </>
   );
+};
+
+ArticleCard.propTypes = {
+  article: PropTypes.object,
 };
 
 export default ArticleCard;
--- src/components/ArticleHero/ArticleHero.jsx
+++ src/components/ArticleHero/ArticleHero.jsx
@@ -3,20 +3,32 @@ import ArticleCard from "../ArticleCard/ArticleCard";
 
 const ArticleHero = () => {
   const [Articles, setArticles] = useState();
+  const [url, setUrl] = useState(
+    "https://api.spaceflightnewsapi.net/v4/articles/?format=json&limit=6"
+  );
   useEffect(() => {
-    const fetchArticles = async () => {
+    const fetchArticles = async (url) => {
       try {
-        const response = await fetch(
-          "https://api.spaceflightnewsapi.net/v4/articles/?format=json&limit=6&offset=6"
-        );
+        const response = await fetch(url);
         const data = await response.json();
         setArticles(data);
       } catch (error) {
         console.log("ERROR: ", error);
       }
     };
-    fetchArticles();
-  }, []);
+    fetchArticles(url);
+  }, [url]);
+
+  const [pageCount, setPageCount] = useState(1);
+
+  const handleNextClick = (url) => {
+    setUrl(url);
+    setPageCount(pageCount + 1);
+  };
+  const handlePreviousClick = (url) => {
+    setUrl(url);
+    setPageCount(pageCount - 1);
+  };
   return (
     <div>
       <div className="container mx-auto px-5 py-10">
@@ -24,15 +36,26 @@ const ArticleHero = () => {
           Latest Articles
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {Articles?.results.map((article) => {
-            <ArticleCard key={article.id} article={article}></ArticleCard>;
-          })}
+          {Articles?.results.map((article) => (
+            <ArticleCard key={article.id} article={article} />
+          ))}
         </div>
-        <div className="flex justify-around mt-8">
-          <button className="bg-accentColorHover hover:bg-gray-500 text-white px-4 py-2 rounded">
+        <div className="flex justify-around mt-8 items-center">
+          <button
+            onClick={() => handlePreviousClick(Articles?.previous)}
+            className="bg-accentColorHover hover:bg-gray-500 text-white px-4 py-2 rounded disabled:bg-gray-500"
+            disabled={Articles?.previous === null}
+          >
             <i className="fa-solid fa-angles-left"></i>
           </button>
-          <button className="bg-accentColorHover hover:bg-gray-500 text-white px-4 py-2 rounded">
+          <h3 className="text-2xl font-bold text-white mb-6 text-center px-4 py-2">
+            {pageCount}
+          </h3>
+          <button
+            onClick={() => handleNextClick(Articles?.next)}
+            className="bg-accentColorHover hover:bg-gray-500 text-white px-4 py-2 rounded disabled:bg-gray-500"
+            disabled={Articles?.next === null}
+          >
             <i className="fa-solid fa-angles-right"></i>
           </button>
         </div>
